5df5778585322cf7fb3e493cc351c6c902e0afcc
voidSatisfaction <lourie@naver.com>
2016-12-13 16:56:04 +0900

    fix 10835 solv

--- review/baekjoon_10835_yeon.cpp
+++ review/baekjoon_10835_yeon.cpp
@@ -12,14 +12,15 @@ int main()
 {
   scanf("%d", &N);
   for(int i=0; i < N; i++)
-    scanf(" %d", &left[i]);
+    scanf(" %d", &left[i])
+    ;
   for(int i=0; i < N; i++)
     scanf(" %d", &right[i]);
 
-  for(int i=1; i <=N ; i++)
+  for(int i=0; i < N ; i++)
   {
-    if(right[i-1] < left[0]) {
-      dp[0][i] = dp[0][i-1] + right[i-1];
+    if(right[i] < left[0]) {
+      dp[0][i+1] = dp[0][i] + right[i];
     } else {
       break;
     }
@@ -38,7 +39,7 @@ int main()
       } else if(l == N){
         dp[l][r] = max(dp[l-1][r-1],dp[l-1][r]);
       } else if(right[r-1] < left[l]){
-        dp[l][r] = dp[l][r-1]+right[r-1];
+        dp[l][r] = max(dp[l][r-1]+right[r-1],dp[l-1][r]);
       } else {
         dp[l][r] = max(dp[l-1][r-1],dp[l-1][r]);
       }
@@ -48,12 +49,14 @@ int main()
   int max = 0;
   for(int l=0; l <= N ; l++)
   {
-    for(int r=0; r <= N ; r++)
-    {
-      if((l == N || r == N) && (dp[l][r] > max))
-        {
-          max = dp[l][r];        
-        }
+    if(dp[l][N] > max){
+      max = dp[l][N];
+    }
+  }
+  for(int r=0; r <= N; r++)
+  {
+    if(dp[N][r] > max){
+      max = dp[N][r];
     }
   }
   
